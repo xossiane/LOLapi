b5263d8bf8e826b67cfbc3db7444355cee935e0a
xossiane <anejosi.lima@gmail.com>
2022-10-21 17:24:10 -0300

    Fix: className added to classList.push

--- components/atoms/ButtonNav/index.jsx
+++ components/atoms/ButtonNav/index.jsx
@@ -4,6 +4,9 @@ import Text from '../Text/index.jsx'
 export default function ButtonNav({ mt, className, children }) {
         const classList = [];
         classList.push(styles[`buttonNav--${mt}`]);
+        classList.push(styles[`buttonNav`]);
+        
+
 
 
   return (<button className={`${classList.join(" ")} ${className}`} ><Text>{children}</Text></button>)
