370dc9fb55e900ada1e39920dfc84ec13c57696c
xossiane <anejosi.lima@gmail.com>
2022-10-21 17:25:15 -0300

    Add: insertinc components and styles to its page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import ButtonNav from '../components/atoms/ButtonNav';
 import Text from '../components/atoms/Text'
 import HomeScreenBackground from '../components/molecules/HomescreenBackground/index.jsx';
-
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div>
     <Text color="white">hello human</Text>
-    <ButtonNav>this is a button</ButtonNav>
+    <ButtonNav><Text color="white" size="large" className={styles[`ButtonNav_Text`]}>PLAY NOW</Text></ButtonNav>
     
     <HomeScreenBackground></HomeScreenBackground>
     </div>
