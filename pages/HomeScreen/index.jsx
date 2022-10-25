c15afb856c66de595eb03127284485802c9a3afe
xossiane <anejosi.lima@gmail.com>
2022-10-25 16:21:23 -0300

    Add: building HomeScreen page

--- pages/HomeScreen/index.jsx
+++ pages/HomeScreen/index.jsx
@@ -1,16 +1,21 @@
-
-
-
+import Text from "../../components/atoms/Text";
+import ButtonNav from "../../components/atoms/ButtonNav";
 import HomeScreenBackground from "../../components/molecules/HomescreenBackground";
+import NavBar from "../../components/organisms/NavBar";
 import styles from "./HomeScreen.module.scss";
 
 export default function HomeScreen() {
   return (
     <div>
-        <HomeScreenBackground></HomeScreenBackground>
-        <img className={styles[`logo`]} src="images/LOL_logo.png"/>
+      <NavBar></NavBar>
+      <HomeScreenBackground></HomeScreenBackground>
+      <section className={styles[`HomeScreen__Container`]}>
+        <img className={styles[`logo`]} src="images/LOL_logo.png" />
 
+        <ButtonNav className={styles[`buttonNav--mt`]} mt="mt">
+          <Text color="white">PLAY FOR FREE</Text>
+        </ButtonNav>
+      </section>
     </div>
-    
   );
 }
