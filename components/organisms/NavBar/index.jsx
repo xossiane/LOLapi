47339da79a253313bda975c31fec439f10539ac1
xossiane <anejosi.lima@gmail.com>
2022-10-26 18:27:41 -0300

    Add: inserting elements in navbar as a list of objects

--- components/organisms/NavBar/index.jsx
+++ components/organisms/NavBar/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 import ButtonNav from '../../atoms/ButtonNav';
+import DropDown from '../../atoms/DropDown';
 import Text from '../../atoms/Text';
 import styles from "./NavBar.module.scss";
+import { NavItens } from './NavItens.js'
 
 function NavBar() {
     
   return (
+  <>
         <nav className={styles[`NavBar`]}>
             
             <img className={styles[`NavBar--imgRiot`]} href="https://www.riotgames.com/pt-br" src="images/RIOTlogo.png"/>
@@ -13,27 +18,31 @@ function NavBar() {
             
             <img className={styles[`NavBar--imgLogo`]}  href="https://universe.leagueoflegends.com/pt_BR/" src="images/LOL_logomini.png"/>
             
-        <section className={styles[`NavBar--items`]}>
-          <li>
-            <a href="/">
-                <Text color="white" size="large">ABOUT</Text>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-            <Text color="white" size="large">CHAMPIONS</Text>
-            </a>
-          </li>
-          <li>
-            <a href="/"><Text color="white" size="large">NEWS</Text></a>
-          </li>
-          <li>
-            <a href="/"><Text color="white" size="large">PROFILE</Text></a>
-          </li>
+        <section className={styles[`NavBar--items`]} >
+          
+          <ul className={styles[`NavBar--items`]}>
+            {NavItens?.map((item) => {
+              return(
+              <li key={item.id} className={item.cName} >
+                <Text color="white" size="large">{item.title}</Text>
+              </li>
+
+              
+              )
+              
+              
+              })}
+          </ul>
+      
           </section>
 
           <ButtonNav><Text color="white" size="medium">PLAY NOW</Text></ButtonNav>
+          
         </nav>
+        
+        <DropDown></DropDown>
+        </>
+        
     
 )}
 
