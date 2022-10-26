94de008dbab1713da0415f390be10a04eb892838
xossiane <anejosi.lima@gmail.com>
2022-10-26 18:27:09 -0300

    Add: NavItens as a list

---
+++ components/organisms/NavBar/NavItens.js
@@ -0,0 +1,37 @@
+export const NavItens = [{
+    id: 1,
+    title: "ABOUT",
+    path: "./about",
+    cName: ""
+    },
+    {   id: 2,
+        title: "CHAMPIONS",
+        path: "./champions",
+        cName: "",
+    },
+    {   id: 3,
+        title: "NEWS",
+        path: "./news",
+        cName: "",
+    },
+    {   id: 4,
+        title: "PROFILE",
+        path: "./profile",
+        cName: "",
+    }
+]
+
+export const DropdownItems = 
+ [
+    {   id: 1,
+        title: "Maps",
+        path: "./maps"
+    },
+    {   id: 2,
+        title: "Creators",
+        path: "./creators"
+    },
+    {   id: 3,
+        title: "Lore",
+        path: "./lore"
+    }]
