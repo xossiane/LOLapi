e96104562f359ef79373e0ea66729d1302ceb3b7
xossiane <anejosi.lima@gmail.com>
2022-11-01 20:47:21 -0300

    Add: next routes to navbar

--- components/organisms/NavBar/index.jsx
+++ components/organisms/NavBar/index.jsx
@@ -1,59 +1,68 @@
-import {React, useState }from 'react'
-
-
-import ButtonNav from '../../atoms/ButtonNav';
-import DropDown from '../../atoms/DropDown';
-import Text from '../../atoms/Text';
+import { React, useState } from "react";
+import Link from "next/link"
+import ButtonNav from "../../atoms/ButtonNav";
+import DropDown from "../../atoms/DropDown";
+import Text from "../../atoms/Text";
 import styles from "./NavBar.module.scss";
-import { NavItens } from './NavItens.js'
+import { NavItens } from "./NavItens.js";
 
 function NavBar() {
-    const [dropDown, setDropDown] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
   return (
-  <>
-        <nav className={styles[`NavBar`]}>
-            
-            <img className={styles[`NavBar--imgRiot`]} href="https://www.riotgames.com/pt-br" src="images/RIOTlogo.png"/>
-            
-            
-            <img className={styles[`NavBar--imgLogo`]}  href="https://universe.leagueoflegends.com/pt_BR/" src="images/LOL_logomini.png"/>
-            
-        <section className={styles[`NavBar--items`]} >
+    <>
+      <nav className={styles[`NavBar`]}>
+        <Link href="https://www.riotgames.com/pt-br"> 
+        <img
+          className={styles[`NavBar--imgRiot`]}
           
+          src="images/RIOTlogo.png"
+        />
+        </Link>
+        <Link href="https://universe.leagueoflegends.com/pt_BR/">
+        <img
+          className={styles[`NavBar--imgLogo`]}
+          
+          src="images/LOL_logomini.png"
+        />
+        </Link>
+
+        <section className={styles[`NavBar--items`]}>
           <ul className={styles[`NavBar--items`]}>
             {NavItens?.map((item) => {
-              if (item.title === "ABOUT"){
-                return(
-                <li key={item.id} className={item.cName} onMouseEnter={() => setDropDown(true)}  onMouseLeave={() => setDropDown(false)}>
-                 
-                <Text color="white" size="large" >{item.title}</Text>
-                {dropDown && <DropDown/>}
-                
-              </li>);
+              if (item.title === "ABOUT") {
+                return (
+                  <li
+                    key={item.id}
+                    className={item.cName}
+                    onMouseEnter={() => setDropDown(true)}
+                    onMouseLeave={() => setDropDown(false)}
+                  >
+                    <Text color="white" size="large">
+                      {item.title}
+                    </Text>
+                    {dropDown && <DropDown />}
+                  </li>
+                );
               }
 
-              return(
-              <li key={item.id} className={item.cName} >
-                <Text color="white" size="large">{item.title}</Text>
-              </li>
-
-              
-              )
-              
-              
-              })}
+              return (
+                <li key={item.id} className={item.cName}>
+                  <Text color="white" size="large">
+                    {item.title}
+                  </Text>
+                </li>
+              );
+            })}
           </ul>
-      
-          </section>
+        </section>
+        <ButtonNav>
+          <Text color="white" size="medium">
+            PLAY NOW
+          </Text>
+        </ButtonNav>
+      </nav>
+    </>
+  );
+}
 
-          <ButtonNav><Text color="white" size="medium">PLAY NOW</Text></ButtonNav>
-          
-        </nav>
-        
-        
-        </>
-        
-    
-)}
-
-export default NavBar
+export default NavBar;
