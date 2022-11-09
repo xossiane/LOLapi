950a610693fb1cfa073da5226a16c6d895ca8bb7
xossiane <anejosi.lima@gmail.com>
2022-11-08 22:09:44 -0300

    Add: background as image props (needs to fix)

--- components/atoms/Background/index.jsx
+++ components/atoms/Background/index.jsx
@@ -4,7 +4,7 @@ import styles from "./Background.module.scss";
 function Background() {
   return (
     <>
-    <img className={styles[`BackgroundImg`]} src="images/Homescreen.png"/>
+    <img className={styles[`BackgroundImg`]} src={"images/Homescreen.png"}/>
     <svg className={styles[`frame`]} width="1223" height="527" viewBox="0 0 1223 527" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1221 525H2V2H1168.62L1221 58.6112V525Z" stroke="#B7B7B7" stroke-width="3"/>
 </svg>
